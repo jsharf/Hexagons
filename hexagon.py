453eeffdc350a3c2483b8917d484c4ae48322140
Jacob Sharf <jacobsharf@gmail.com>
2015-05-16 00:02:17 -0700

    Hexagon rendering to PNG is complete! implemented Bresenham's line drawing algorithm and gaussian blur to make a nice, clean image

--- hexagon.py
+++ hexagon.py
@@ -1,7 +1,43 @@
 from math import sin, cos, sqrt
-PI = 3.1415926535
 import matplotlib.pyplot as plt
+import numpy.random as rnd
+import numpy as np
 import png
+
+PI = 3.1415926535
+
+Xdim = 1000
+Ydim = 1000
+
+bitmap = [[0 for _ in range(Ydim)] for _ in range(Xdim)]
+
+def GaussianMatrix(radius, stddev):
+    mat = [[0 for _ in range(radius*2 + 1)] for _ in range(radius*2 + 1)]
+    for i in range(radius*2 + 1):
+        for j in range(radius*2 + 1):
+            rad = sqrt((i - radius)**2 + (j - radius)**2)
+            mat[i][j] = np.exp(-(rad**2)/(2*(stddev**2)))
+    return mat
+
+def blur(radius, stddev):
+    global bitmap
+    newmap = [[x for x in row] for row in bitmap]
+    mat = GaussianMatrix(radius, stddev)
+    for x in range(Xdim):
+        for y in range(Ydim):
+            left = max(x - radius, 0)
+            right = min(x + radius, Xdim)
+            top = max(y - radius, 0)
+            bottom = min(y + radius, Ydim)
+            total = 0
+            for px in range(left, right):
+                for py in range(top, bottom):
+                    val = bitmap[px][py] 
+                    # calculate x and y parameters for gaussian
+                    gradius = sqrt(x**2 + y**2)
+                    total += val*mat[px - left][py - top]
+            newmap[x][y] = min(int(total), 255)
+    bitmap = newmap
 
 class Coordinate:
     def __init__(self, x=0, y=0, z=0):
@@ -21,7 +57,7 @@ class Coordinate:
     def mult(self, c):
         return Coordinate(self.x*c, self.y*c, self.z*c)
     def div(self, c):
-        return self.mult(1/c)
+        return self.mult(1.0/c)
     def norm(self):
         magnitude = self.mag()
         return self.div(magnitude)
@@ -36,24 +72,72 @@ class Coordinate:
     def __repr__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ")"
 
+center = Coordinate(Xdim/2.0, Ydim/2.0, 0)
 
-Xdim = 1000
-Ydim = 1000
-bitmap = Xdim*[Ydim*[0]]
+def switchToOctantZeroFrom(octant, x, y):
+    return [(x, y), (y, x), (y, -x), 
+            (-x, y), (-x, -y), (-y, -x), (-y, x),
+            (x, -y)][octant]
 
-center = Coordinate(Xdim/2, Ydim/2, 0)
+def switchToOctantFromZero(octant, x, y):
+    return [(x, y), (y, x), (-y, x), 
+            (-x, y), (-x, -y), (-y, -x), (y, -x),
+            (x, -y)][octant]
+
+def sign(x):
+    if (x >= 0):
+        return 1
+    else:
+        return -1
+
+def octant(x, y):
+    qd = {
+            (-1, -1) : 4 if (y > x) else 5,
+            (-1, 1)  : 2 if (y > abs(x)) else 3,
+            (1, -1)  : 6 if (abs(y) > x) else 7,
+            (1, 1)   : 1 if (y > x) else 0,
+        }
+    ret = qd[(sign(x), sign(y))]
+    #print(ret)
+    return ret
+
+
+def zeroLine(zero, xp, yp):
+    # convert to octant zero for Bresenham's line algorithm
+    (xo, yo) = (xp - zero[0], yp - zero[1])
+    o = octant(xo, yo)
+    (x, y) = switchToOctantZeroFrom(o, xo, yo)
+    dx = float(x)
+    dy = float(y)
+    error = float(0)
+    derr = abs(float(dy/dx))
+    print(o)
+    print((xo, yo))
+    print((int(x), int(y)))
+    y = int(0)
+    for xi in range(0, int(x)):
+        (px, py) = switchToOctantFromZero(o, xi, y)
+        bitmap[int(zero[0] + px)][int(zero[1] + py)] = 255
+        #print((int(zero[0] + px), int(zero[1] + py)))
+        error += derr
+        while (error >= 0.5):
+            (px, py) = switchToOctantFromZero(o, xi, y)
+            bitmap[int(zero[0] + px)][int(zero[1] + py)] = 255
+            #print((int(zero[0] + px), int(zero[1] + py)))
+            y += sign(dy)
+            error -= 1.0
+
+def line(x0, y0, x1, y1):
+    zeroLine((y0, x0), y1, x1)
 
 def draw(p1, p2):
-    startc = center + p1
-    vec = (p2 + p1.mult(-1)).norm().div(100.0)
-    for i in range(int(100*(p2 + p1.mult(-1)).mag())):
-        point = startc + vec.mult(i)
-        x = int(point.x)
-        y = int(point.y)
-        bitmap[x][y] = 1
+    line(p1.x + center.x, p1.y + center.y, p2.x + center.x, p2.y + center.y)
+
+def line_center(x0, y0, x1, y1):
+    line(x0 + center.x, y0 + center.y, x1 + center.x, y1 + center.y)
 
 def render():
-    pic = png.from_array(bitmap, mode='L;1')
+    pic = png.from_array(bitmap, mode='L')
     return pic
 
 class Cube:
@@ -76,7 +160,6 @@ class Cube:
         height = 2.0*self.gridSize
         basisr = Coordinate(sqrt(3.0)/2.0, 0, 0).mult(height)
         basisq = Coordinate(sqrt(3.0)/4.0, 0.75, 0).mult(height)
-        print(self.loc)
         center = basisr.mult(self.loc.y) + basisq.mult(self.loc.z)
         edge = self.gridSize
         return Hexagon(center.x, center.y, edge)
@@ -94,6 +177,7 @@ class Hexagon:
         for i in range(6):
             p1 = self.getCorner(i)
             p2 = self.getCorner(i + 1)
+            print((p1.x, p1.y, p2.x, p2.y))
             draw(p1, p2)    
     def Cube(self):
         height = self.edge*2
@@ -104,12 +188,10 @@ class Hexagon:
         x = remaining.proj(basisr)
         return Cube(Coordinate(x.mag(), y.mag()), self.edge) 
 
-A = Cube(Coordinate(0, 0), 1)
-B = A.getNeighbor('LD')
-C = A.getNeighbor('RD')
-B.hexagon().draw()
-C.hexagon().draw()
+A = Cube(Coordinate(0, 0), 50)
 A.hexagon().draw()
+
+#blur(5, 0.8)
 
 pic = render()
 pic.save('output.png')
